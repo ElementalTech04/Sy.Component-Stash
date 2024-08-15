68e8857f1c93455b4dfdef6e70e7d37a6ef32fe0
Frankie Rodriguez <rfrankie@gmail.com>
2024-08-15 18:33:58 -0400

    added form prompt to question ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 
-class Main extends HTMLElement {
-    constructor() {
-
-        super();
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.innerHTML = `
-            <style>
-                :host {
-                    display: block;
-                }
-                
-                #container {
-                    
-                        
-                        display: flex;
-                        flex-direction: column;
-                        align-items: center;
-                        justify-content: center;
-                        height: 100vh;
-                        width: 100vw;
-                    
-                }
-            </style>
-            <div id="container">
-                
-                    
-                        <h1>Hello World</h1>
-                        
-                        
-                            <p>This is a paragraph</p>
-                            <p>This is another paragraph</p>
-                            <p>This is the last paragraph</p>
-                        
-                    
-                
-            </div>
-        `;
-
-
-
-    }
-}
-
-export default Main;
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <></>
+    </StrictMode>,
+)
